c2ccc65d533efe42b783f7c44f3aa38c6a702747
Daniel S. Billing <daniel@izy.no>
2023-09-06 23:20:31 +0200

    Update setup details

--- setup.py
+++ setup.py
@@ -9,29 +9,29 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name="oppe",
+    name='oppe',
     version='0.0.1',
-    description="A Python API Wrapper for Oppe",
+    description='A Python API Wrapper for Oppe',
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     project_urls={
         'Homepage': 'https://oppe.app',
-        'Documentation': 'https://oppe.app',
-        'Github': 'https://github.com/kilobyteno/oppe.py'
+        'Documentation': 'https://docs.oppe.app',
+        'Github': 'https://github.com/kilobyteno/oppe-for-python',
     },
-    url="https://github.com/kilobyteno/oppe.py",
-    author="Kilobyte AS",
-    author_email="daniel@kilobyte.no",
-    license="MIT",
+    url='https://github.com/kilobyteno/oppe-for-python',
+    author='Kilobyte AS',
+    license='MIT',
     python_requires='>=3.6',
-    packages=["oppe"],
+    packages=['oppe'],
     include_package_data=True,
     install_requires=requirements,
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ]
 )

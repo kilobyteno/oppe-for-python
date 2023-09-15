bb3a957fe59aa0e437982b0e6f798ace53f51b6f
Daniel S. Billing <daniel@izy.no>
2023-09-15 17:07:47 +0200

    Refactored triggering of events
    - Added emoji
    - Added data
    - Made description optional
    - Updated readme with event triggering

--- README.md
+++ README.md
@@ -9,4 +9,21 @@ pip install oppe
 ```
 
 ## Usage
-*to be added*
+
+### Sending an event
+
+```python
+from oppe import Oppe
+
+oppe = Oppe(api_token='insert-api-token-here', project_id="uuid-of-project")
+
+oppe.event(
+    channel_id="uuid-of-channel",
+    title="user-registered",
+    description="A new user has registered.",
+    emoji="👋",
+    data={
+        "user_id": 123,
+    },
+)
+```
--- oppe/oppe.py
+++ oppe/oppe.py
@@ -3,17 +3,18 @@ import json
 import requests
 
 from oppe.config import Config
+from oppe.utils import request_header
 
 
 class Oppe:
     """
-    Oppe class for triggering events in the Oppe app.
+    Oppe class for communicating with the Oppe API.
 
     Attributes:
         api_token (str):
             The authentication api_token.
         project_id (str):
-            The project_id identifier.
+            The project_id uuid identifier.
     """
 
     def __init__(self, api_token, project_id):
@@ -22,35 +23,36 @@ class Oppe:
 
         :param api_token: The authentication api_token.
         :type api_token: str
-        :param project_id: The project_id identifier.
+        :param project_id: The project_id uuid identifier.
         :type project_id: str
         """
         self.api_token = api_token
         self.project_id = project_id
 
-    def trigger_event(self, channel, title, description):
+    def event(self, channel_id, title, description=None, emoji=None, data=None):
         """
-        Triggers an event in the Oppe app.
+        Send an event to the Oppe app.
 
-        :param channel: The channel identifier.
-        :type channel: str
-        :param title: The title of the event.
+        :param channel_id: The channel uuid identifier.
+        :type channel_id: str
+        :param title: The title of the event. Example: "user-registered"
         :type title: str
-        :param description: The description of the event.
+        :param description: The description of the event. Example: "A new user has registered."
         :type description: str
+        :param emoji: An emoji to be displayed with the event. Example: "👋"
+        :type emoji: str
+        :param data: Any additional data to be sent with the event. Example: {"user_id": 1}
+        :type data: object
 
         :return: True if the event was triggered successfully, False otherwise.
         :rtype: bool
         """
         data = {
-            'channel_id': channel,
+            'channel_id': channel_id,
             'title': title,
             'description': description,
+            'emoji': emoji,
+            'data': data
         }
-        headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.api_token}'
-        }
-        requests.post(Config.EVENT_URL, data=json.dumps(data), headers=headers)
+        requests.post(Config.EVENT_URL, data=json.dumps(data), headers=request_header(api_token=self.api_token))
         return True

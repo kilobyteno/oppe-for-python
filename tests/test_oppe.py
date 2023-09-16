460c76c478fba80604bbce26702e6d993026073c
Daniel S. Billing <daniel@izy.no>
2023-09-16 14:28:05 +0200

    Added test for invalid api token

--- tests/test_oppe.py
+++ tests/test_oppe.py
@@ -1,9 +1,11 @@
 import os
+from unittest.mock import Mock
 
+import pytest
 from dotenv import load_dotenv
 from faker import Faker
 
-from oppe.exceptions import UuidValidationError
+from oppe.exceptions import EventRequestError, UuidValidationError
 from oppe.oppe import Oppe
 
 fake = Faker()
@@ -82,3 +84,16 @@ def test_publish_event_with_wrong_channel_id():
         oppe.event(channel_id=fake.sha256(), title=fake.domain_word())
     except UuidValidationError as e:
         assert e.msg == 'Channel ID must be a valid UUID'
+
+
+def test_invalid_api_token(mocker):
+    """ Test invalid api token """
+    # Mock the requests.post method to return a non-201 response
+    mocker.patch('requests.post', return_value=Mock(status_code=401, json=lambda: {'error': 'Unauthorized'}))
+
+    # Create an instance of the Oppe class
+    oppe = Oppe(api_token='invalid_token', project_id=os.getenv('TEST_PROJECT_ID'))
+
+    # Call the event method with valid parameters
+    with pytest.raises(EventRequestError):
+        oppe.event(channel_id=os.getenv('TEST_CHANNEL_ID'), title=fake.domain_word())
